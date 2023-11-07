7d9ab3e93da009de600c00e928e8e72eb14bfe0c
aashimap <aashima@bititude.com>
2023-11-07 09:38:59 +0530

    coop policy resolve

--- server.js
+++ server.js
@@ -10,12 +10,17 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    //"http://localhost:3000",
-    origin: "https://fun-learn-app.netlify.app",
+    origin: "http://localhost:3000",
+    //"https://fun-learn-app.netlify.app",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
 );
+
+app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
+  next();
+});
 
 require("dotenv").config();
 
